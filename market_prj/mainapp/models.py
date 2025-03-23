a63f94d4e78eda5c26431e91cf4b2fcc67e753dd
Vasiliy <v_derkach@inbox.ru>
2025-03-23 20:14:35 +0500

    5

---
+++ market_prj/mainapp/models.py
@@ -0,0 +1,69 @@
+import uuid
+# Create your models here.
+# from django.conf import settings
+
+from django.conf import settings
+from django.db import models
+# from aldjemy.meta import AldjemyMeta
+# from sqlalchemy import create_engine
+# SQLALCHEMY_ENGINES = {
+#     "postgresql": "postgresql",
+#     # "postgresql_psycopg2": "postgresql+psycopg2",
+#     }
+# from aldjemy.meta import AldjemyMeta# from django.contrib.postgres.fields import ArrayField, DateRangeField
+class ListOfCountries(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(verbose_name='имя', max_length=64, unique=True)
+    description = models.TextField(verbose_name='описание', blank=True)
+    is_active = models.BooleanField(verbose_name='активна', default=True)
+
+    def __str__(self):
+        return self.name
+
+class Regions(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    country = models.ForeignKey(ListOfCountries, on_delete=models.CASCADE)
+    name = models.CharField(verbose_name='имя', max_length=64, unique=True)
+    description = models.TextField(verbose_name='описание', blank=True)
+    is_active = models.BooleanField(verbose_name='активна', default=True)
+
+    def __str__(self):
+        return self.name
+
+class Accommodation(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    country = models.ForeignKey(ListOfCountries, on_delete=models.CASCADE)
+    region = models.ForeignKey(Regions, on_delete=models.CASCADE, related_name='Regionid')
+    name = models.CharField(verbose_name='название проживания',
+                                           max_length=128, unique=True)
+    image = models.ImageField(upload_to='accommodation_img', blank=True)
+    short_desc = models.TextField(verbose_name='краткое описание продукта',
+                                  max_length=60, blank=True)
+    description = models.TextField(verbose_name='описание продукта',
+                                   blank=True)
+    availability = models.PositiveIntegerField(
+				verbose_name='количество свободных номеров')
+    price = models.DecimalField(
+        verbose_name='цена', max_digits=8, decimal_places=2, default=0)
+    room_desc = models.TextField(verbose_name='краткое описание комнаты',
+                                 max_length=60, blank=True)
+    is_active = models.BooleanField(verbose_name='активна', default=True)
+
+    @staticmethod
+    def get_items():
+        reslt = Accommodation.objects.filter(is_active=True).order_by('country','regions','name')
+        return reslt
+
+    @staticmethod
+    def get_country_items(country_id):
+        # list_of_accommodations = Accommodation.sa.query()
+        # list_of_accommodations1 = list_of_accommodations.join(Regions.sa,Accommodation.sa.region.has(Regions.sa.id))
+        # list_of_accommodations2 = list_of_accommodations1.filter(Regions.sa.country.has(uuid.UUID(country_id))).all()
+        # # list_of_accommodations2 = list_of_accommodations1.filter(Regions.sa.country.has(uuid(country_id)))
+        list_of_accommodations2 = Accommodation.objects.select_related('region').filter(region_id__country_id=uuid.UUID(country_id)).all()
+
+        return list_of_accommodations2
+
+
+    def __str__(self):
+        return f'{self.name} ({self.country.name})'
---
+++ market_prj/mainapp/views.py
@@ -0,0 +1,63 @@
+from django.shortcuts import render
+from django.contrib import auth
+import uuid
+import aldjemy
+# from market_prj.market_prj.authapp import models
+from authapp.models import TravelUser,TravelUserProfile
+import authapp
+def main(request):
+    return render(request, 'mainapp/index.html')
+
+# Create your views here.
+
+from .models import Accommodation
+def accommodations(request):
+    title = 'размещение'
+    format = 'icons'
+    curent_user = auth.get_user(request)
+    profile_curr_user = TravelUserProfile.objects.filter(user=curent_user.id)
+    # print(list(profile_curr_user.values('accomm_format')),curent_user.id)
+    curr_profile =  profile_curr_user.values('accomm_format')
+    if curr_profile:
+        format = curr_profile[0]['accomm_format']
+    if request.method == 'POST':
+        # print(request.POST)
+        if request.POST.get('btn_format'):
+            # print('1')
+            format = request.POST.get('format')
+        if request.POST.get('btn_table'):
+            # print('2')
+            format = 'table'
+        if request.POST.get('btn_icons'):
+            # print('3')
+            format = 'icons'
+        # user.accomm_format = format
+        # user.save()
+        TravelUserProfile.objects.filter(user=curent_user.id).update(accomm_format=format)
+        # print(btn_format)
+    # list_of_accommodations = Accommodation.objects.filter(is_active=True)
+    list_of_accommodations = Accommodation.get_country_items('00000000-0000-0000-0000-000000000001')
+
+    # for a in list_of_accommodations:
+    #     print('accomm=',a)
+
+    content = {
+        'title': title,
+        'list_of_accommodations': list_of_accommodations,
+        'format': format,
+    }
+
+    return render(request, 'mainapp/accommodations.html', content)
+
+from django.shortcuts import get_object_or_404
+from .models import ListOfCountries
+
+def accommodation(request, pk):
+    title = 'продукты'
+
+    content = {
+        'title': title,
+        'accommodation': get_object_or_404(Accommodation, pk=pk),
+    }
+
+    return render(request, 'mainapp/accommodation_details.html', content)
