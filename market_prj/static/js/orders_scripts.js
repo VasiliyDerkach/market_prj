ea768af40596190ec766b054c2c3265480bcb019
Vasiliy <v_derkach@inbox.ru>
2025-05-14 21:38:02 +0500

    140525

--- market_prj/static/js/orders_scripts.js
+++ market_prj/static/js/orders_scripts.js
@@ -2,13 +2,13 @@ window.onload = function () {
     $('.order_form').on('click','input[name^="orderitems-"]', function () {
         var t_href = event.target;
         var vlu = t_href.value;
-        var exs= $('#id_order_exist').value();
+        var exs= $('#id_order_exist').attr('value');
         if (vlu) {
             var vlu = 'vlu'
         }
         else {
             var vlu = 'none'
-        }
+        };
         $.ajax({
             url: "/order/edit_accommodation/" + exs + '/' + t_href.name+'/'+t_href.value+'/' ,
 
@@ -22,13 +22,13 @@ window.onload = function () {
     $('.order_form').on('change','select[name^="orderitems-"]', function () {
         var t_href = event.target;
         var vlu = t_href.value;
-        var exs= $('#id_order_exist').value();
+        var exs= $('#id_order_exist').attr('value');
         if (vlu) {
             var vlu = vlu
         }
         else {
             var vlu = 'none'
-        }
+        };
         $.ajax({
             url: "/order/edit_accommodation/" + exs + '/' + t_href.name+'/'+vlu+'/' ,
 
