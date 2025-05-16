8e7001daf59045aacdf3a94c8971b773679d2930
1 <v_derkach@inbox.ru>
2025-05-16 15:00:11 +0500

    160525+

--- market_prj/static/js/orders_scripts.js
+++ market_prj/static/js/orders_scripts.js
@@ -3,20 +3,20 @@ window.onload = function () {
         var t_href = event.target;
         var vlu = t_href.value;
 //        извлечь номер строки из t_href
-        var idx = t_href.replace('orderitems-','');
+        var idx = t_href.name.replace('orderitems-','');
         var idx = idx.slice(0,idx.indexOf('-'));
-        var exs= $('#orderitems-'+idx+'-id').attr('value');
+        var exs= $('#id_orderitems-'+idx+'-id').attr('value');
         if (exs) {
-            var exs = exs
+            var exs = exs;
         }
         else {
-            var exs = '-'
+            var exs = '-';
         };
         if (vlu) {
-            var vlu = vlu
+            var vlu = vlu;
         }
         else {
-            var vlu = 'none'
+            var vlu = 'none';
         };
         $.ajax({
             url: "/order/edit_accommodation/" + exs + '/' + t_href.name+'/'+vlu+'/' ,
@@ -31,20 +31,20 @@ window.onload = function () {
     $('.order_form').on('change','select[name^="orderitems-"]', function () {
         var t_href = event.target;
         var vlu = t_href.value;
-        var idx = t_href.replace('orderitems-','');
+        var idx = t_href.name.replace('orderitems-','');
         var idx = idx.slice(0,idx.indexOf('-'));
-        var exs= $('#orderitems-'+idx+'-id').attr('value');
+        var exs= $('#id_orderitems-'+idx+'-id').attr('value');
         if (exs) {
-            var exs = exs
+            var exs = exs;
         }
         else {
-            var exs = '-'
+            var exs = '-';
         };
         if (vlu) {
-            var vlu = vlu
+            var vlu = vlu;
         }
         else {
-            var vlu = 'none'
+            var vlu = 'none';
         };
         $.ajax({
             url: "/order/edit_accommodation/" + exs + '/' + t_href.name+'/'+vlu+'/' ,
